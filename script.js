29acc87e138b1f3196ca1865c690980ff6187cf7
kumokuro <katieannemaher@gmail.com>
2021-12-29 10:04:38 -0600

    more UI adjustments, working on JS logic

--- script.js
+++ script.js
@@ -24,33 +24,97 @@ function operate(operator, num1, num2) {
   return operator(num1, num2);
 }
 
+const numberButtons = document.querySelectorAll("[data-number]");
+const operatorButtons = document.querySelectorAll("[data-operator]");
+const equalsButton = document.querySelector("[data-equals]");
+const clearButton = document.querySelector("[data-clear]");
+const currentOperandText = document.querySelector("[data-current-display]");
+const lastOperandText = "";
+
 class Calculator {
-  constructor(lastOperand, currentOperand) {
-    this.lastOperand = lastOperand;
-    this.currentOperand = currentOperand;
+  constructor(lastOperandText, currentOperandText) {
+    this.lastOperandText = lastOperandText;
+    this.currentOperandText = currentOperandText;
+    this.clear();
   }
 
-  clear() {}
+  clear() {
+    this.currentOperand = "";
+    this.lastOperand = "";
+    this.operator = undefined;
+  }
 
-  delete() {}
+  appendNumber(number) {
+    if (number === "." && this.currentOperand.includes(".")) return;
+    this.currentOperand = this.currentOperand.toString() + number.toString();
+  }
 
-  appendNumber(number) {}
+  chooseOperation(operator) {
+    if (this.currentOperand === "") return;
+    if (this.lastOperand !== "") {
+      this.calculate();
+    }
+    this.operator = operator;
+    this.lastOperand = this.currentOperand;
+    this.currentOperand = "";
+  }
 
-  chooseOperation(operation) {}
+  calculate() {
+    let result = null;
+    const lastNum = parseFloat(this.lastOperand);
+    const currentNum = parseFloat(this.currentOperand);
 
-  compute() {}
+    if (isNaN(lastNum) || isNaN(currentNum)) return;
+    switch (this.operator) {
+      case "+":
+        result = lastNum + currentNum;
+        break;
+      case "-":
+        result = lastNum - currentNum;
+        break;
+      case "*":
+        result = lastNum * currentNum;
+        break;
+      case "÷":
+        result = lastNum / currentNum;
+        break;
+      default:
+        return;
+    }
+    this.currentOperand = result;
+    this.operator = undefined;
+    this.lastOperand;
+  }
 
-  updateDisplay() {}
+  updateDisplay() {
+    this.currentOperandText.innerText = this.currentOperand;
+  }
 }
 
-let result = 0;
+const calculator = new Calculator(lastOperandText, currentOperandText);
 
-// displaying the results to the screen
-const resultDisplay = document.querySelector("#result");
-resultDisplay.textContent = result;
+numberButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.appendNumber(button.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    calculator.chooseOperation(button.innerText);
+    calculator.updateDisplay();
+  });
+});
 
 /* 
+Whatever numbers get pressed before the operand needs to be assigned to lastOperand. 
 
+Append number to currentNum
+Append number to currentNum 
++
+if operator is clicked, last Operand = currentNum 
+clear currentNum 
 
 
 */
